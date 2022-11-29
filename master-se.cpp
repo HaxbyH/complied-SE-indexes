072c0f0964a527b1265d10c069efedcc5c7ef26f
HaxbyH <64629503+HaxbyH@users.noreply.github.com>
2022-11-29 21:00:56 +1300

    Add files via upload

--- master-se.cpp
+++ master-se.cpp
@@ -9,6 +9,15 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+// CHANGE VARIABLES
+const size_t NUMDOCS = 100;
+const char* INDEX_NAME = "./WSJheaders/testdoco.h";
+static double k1 = 0.9;
+static double b = 0.4;
+double documents_in_collection;
+double average_document_length = 0;
+
+
 bool newdoc = false;
 typedef std::vector<std::pair<int32_t, int32_t> > postings;
 std::unordered_map<std::string, postings> vocabulary;
@@ -26,20 +35,27 @@ typedef struct
 	uint32_t postings_list_length;
 	} dictionary;
 
-// to be replaced
-const std::string doc_array[] = {};
-const dictionary vocab[] = {};
+typedef struct
+    {
+    double rsv_sc;
+    std::string term_id;
+    } accumulator;
 
-int n = sizeof(doc_array) / sizeof(doc_array[0]);
-std::vector<std::string> doc_ids(doc_array, doc_array + n);
-int docnum = doc_ids.size()-1;
+// if index exists include 
+#if __has_include("WSJheaders/testdoco.h")
+#include "WSJheaders/testdoco.h"
+#else
+#include "emptyindex.h"
+#endif
+
+std::vector<std::string> doc_ids;
+std::vector<int> doc_lengths;
+int docnum = -1;
 bool doctag = false;
 
-/*
-   Functions takes in a string and seperates words 
-   while removing puncutation 
-*/ 
-
+/* -------------------------
+        TO_LOWERCASE
+   ------------------------*/
 char to_lowercase(char c) 
 {
     if (c >= 'A' && c <= 'Z') {
@@ -48,6 +64,9 @@ char to_lowercase(char c)
     return c;
 }
 
+/* -------------------------
+      REMOVE PUNCTUATION
+   ------------------------*/
 std::string remove_punc(std::string word) {
     std::string temp = "";
     for (int i = 0; i < word.size(); i++) {
@@ -58,8 +77,10 @@ std::string remove_punc(std::string word) {
     return temp;
 }
 
+/* ---------------------------
+        GET NEXT TOKEN
+   --------------------------*/
 std::vector<std::string> get_next(std::string line) {
-
     std::vector<std::string> doc_array;
     std::string delimiter = " ";
     size_t pos = 0;
@@ -117,41 +138,11 @@ std::vector<std::string> get_next(std::string line) {
     return doc_array;
 }
 
-int vocab_compare(const void *a, const void *b) {
-    dictionary *left = (dictionary *)a;
-    dictionary *right = (dictionary *)b;
-
-    return strcmp(left->term, right->term);
-}
-void process_one_term(int16_t *accumulator, const char *word) {
-    dictionary term;
-    term.term = word;
-
-    dictionary *got = (dictionary *) bsearch(&term, vocab, sizeof(vocab) / sizeof(*vocab), sizeof(*vocab), vocab_compare);
-    if (got != NULL) {
-        for (int i = 0; i < got->postings_list_length; i++) {
-            accumulator[got->postings_list[i].document_id] += got->postings_list[i].term_frequency;
-        }
-    }
-}
-
-void search(const char** words, int numWords) {
-    int16_t accumulator[10] = {};
-    // loop through words
-    for (int i = 2; i < numWords; i++) {
-        process_one_term(accumulator, words[i]);
-    }
-    for (int doc = 0; doc < 10; doc++) {
-        if (accumulator[doc] != 0) {
-            std::cout << doc_array[doc] << ": " << accumulator[doc] << std::endl;
-        }
-    }
-}
-
 /* ------------------------
           INDEX
 -------------------------*/
 void index(const char* input) {
+
     std::ifstream file;
     file.open(input);
 
@@ -163,6 +154,7 @@ void index(const char* input) {
     
     // iterate over each line
     std::string line;
+    int idoclength = 0;
     while(getline(file, line)) {
         std::vector<std::string> oneline = get_next(line);
 
@@ -183,17 +175,26 @@ void index(const char* input) {
                 newdoc = true;
             }
 
+            // hit end of file
+            if (token.compare("</DOC>") == 0) {
+                doc_lengths.push_back(idoclength);
+                // std::cout << idoclength << std::endl;
+                idoclength = 0;
+            }
+
             // don't index doc tags
             if (token.at(0) == '<') {
                 continue;
             }
+
+            // increase the doc length
+            idoclength++;
 
             // convert to lowercase
             for (int i = 0; i < token.size(); i ++) {
                 char &c = token[i];
                 c = to_lowercase(c);
             }
-
             // Adding to unordered map
             postings &currentlist = vocabulary[token];
             if (currentlist.empty() || currentlist[currentlist.size()-1].first != docnum) {
@@ -215,20 +216,11 @@ void index(const char* input) {
 
     // put doc_ids in file
     std::ofstream outfile;
-    outfile.open("output.cpp");
+    outfile.open(INDEX_NAME);
 
-    std::ifstream infile;
-    infile.open("master-se.cpp");
-
-    // adding headers to file
-    for (int i = 0; std::getline(infile,line) && i < 29; i++) { //starts writing to line 30
-        outfile << line << "\n";
-    }
-
-    // addings doc_ids
-    outfile << "const std::string doc_array[] = {";
+    outfile << "const std::string doc_array[] = {\n";
     for (int i = 0; i < doc_ids.size(); i++) {
-        outfile << "\"" << doc_ids[i] << "\"" << ",\n";
+        outfile << "\t\"" << doc_ids[i] << "\"" << ",\n";
     }
     outfile << "};\n\n";
 
@@ -245,30 +237,115 @@ void index(const char* input) {
         outfile << "}};\n";
     }
 
-    outfile << "\n";
+    outfile << "\n\n";
 
     // adding vocabulary
     outfile << "const dictionary vocab[] = {\n";
     for (int i = 0; i < keys.size(); i++) {
         outfile << "\t{\"" << keys[i] << "\", i_" << keys[i] << ", " << vocabulary[keys[i]].size() << "},\n";
     }
+    outfile << "};\n\n";
+
+    // adding doc lengths
+    outfile << "const int doclengths[] = {";
+    for (int i = 0; i < doc_lengths.size(); i++) {
+        outfile << doc_lengths[i] << ", ";
+    }
     outfile << "};\n";
     
-    // adding the rest
-    std::getline(infile, line);
-    while(std::getline(infile, line)) {
-        outfile << line << "\n";
-    }
     file.close();
-    infile.close();
     outfile.close();
     
 }
 
-void help() {
-    std::cout << "help selected" << "\n";
+/* -----------------------
+        COMPARE TOKENS
+   -----------------------*/
+int vocab_compare(const void *a, const void *b) {
+    dictionary *left = (dictionary *)a;
+    dictionary *right = (dictionary *)b;
+
+    return strcmp(left->term, right->term);
 }
 
+/* -----------------------
+     ADD TO ACCUMULATOR
+   -----------------------*/
+void process_one_term(accumulator *acc, const char *word) {
+    dictionary term;
+    term.term = word;
+
+    dictionary *got = (dictionary *) bsearch(&term, vocab, sizeof(vocab) / sizeof(*vocab), sizeof(*vocab), vocab_compare);
+
+    // calculate idf
+    int p_length = got->postings_list_length;
+    double idf = log(documents_in_collection / p_length);
+
+    if (got != NULL) {
+        for (int i = 0; i < p_length; i++) {
+            int tf = got->postings_list[i].term_frequency;
+            int d = got->postings_list[i].document_id;
+            double rsv_score = idf * ((tf * (k1 + 1)) / (tf + k1 * (1 - b + b * (doclengths[d] / average_document_length))));
+            // std::cout << rsv_score << std::endl;
+
+            acc[d].rsv_sc += rsv_score;
+        }
+    }
+}
+
+/* ----------------------
+        COMPARE RSV
+-------------------------*/
+int compare_rsv(const void *a, const void *b) {
+    accumulator *accumulatorA = (accumulator *)a;
+    accumulator *accumulatorB = (accumulator *)b;
+    if (accumulatorA->rsv_sc < accumulatorB->rsv_sc) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+/* ----------------------
+        SEARCH
+-----------------------*/
+void search(const char** words, int numWords) {
+
+    // numbers of documents in collection
+    documents_in_collection = sizeof(doclengths) / sizeof(int32_t);
+
+    // average numbers of documents in collection
+    for (int32_t document = 0; document < documents_in_collection; document++)
+	    average_document_length += doclengths[document];
+    average_document_length /= documents_in_collection;
+
+    // accumulator
+    accumulator acc[NUMDOCS];
+
+    // worried about speed here
+    for (int i = 0; i < documents_in_collection; i++) {
+        acc[i].term_id = doc_array[i];
+        acc[i].rsv_sc = 0;
+
+    }
+
+    // // loop through words
+    for (int i = 2; i < numWords; i++) {
+        process_one_term(acc, words[i]);
+    }
+
+    qsort(acc, documents_in_collection, sizeof(accumulator), compare_rsv);
+
+    std::string query_id = "PH_ID";
+    for(int i = 0; i < documents_in_collection && acc[i].rsv_sc != 0.0; i++) {
+        std::cout << query_id << " Q0 " << acc[i].term_id << " " << i+1 << " " << acc[i].rsv_sc << " c-search" << std::endl;
+    };
+
+}
+
+/*---------------------
+        MAIN
+-----------------------*/
 int main(int argc, const char *argv[]) {
 
     const char* operation = argv[1];
@@ -285,12 +362,8 @@ int main(int argc, const char *argv[]) {
     } else if (strcmp(operation, "search")==0) {
         search(argv, argc);
 
-    // help operation is used
-    } else if (strcmp(operation, "help")==0) {
-        help();
-
     // return error message
     } else {
-        std::cout << "error: type - help" << "\n";
+        std::cout << "ERROR" << std::endl;
     }
 }
